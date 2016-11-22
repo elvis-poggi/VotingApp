83f1f37088bb05626eb501ebc9a54b25d08c831e
elvis-poggi <poggi.elvis@gmail.com>
2016-11-22 15:28:36 +0100

    installed websocket

--- voting-client/src/reducer.js
+++ voting-client/src/reducer.js
@@ -4,7 +4,7 @@ function setState(state, newState){
     return state.merge(newState);
 }
 
-export default function(state = Map(), acrtion) {
+export default function(state = Map(), action) {
     switch (action.type) {
     case 'SET_STATE':
         return setState(state, action.state);
